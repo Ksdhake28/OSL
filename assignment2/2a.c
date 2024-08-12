a025d250cdac0858a4069a4fcc29f6ec6def8221
Ksdhake28 <kdhake289@gmail.com>
2024-08-12 08:37:17 +0530

    Assignment2

---
+++ assignment2/2a.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+// Function to perform sorting (Selection Sort used here for demonstration)
+void selectionSort(int arr[], int n) {
+    int i, j, min_idx, temp;
+    for (i = 0; i < n-1; i++) {
+        min_idx = i;
+        for (j = i+1; j < n; j++) {
+            if (arr[j] < arr[min_idx]) {
+                min_idx = j;
+            }
+        }
+        // Swap the found minimum element with the first element
+        temp = arr[min_idx];
+        arr[min_idx] = arr[i];
+        arr[i] = temp;
+    }
+}
+
+void selectionSortDesc(int arr[], int n) {
+    int i, j, min_idx, temp;
+    for (i = 0; i < n-1; i++) {
+        min_idx = i;
+        for (j = i+1; j < n; j++) {
+            if (arr[j] > arr[min_idx]) {
+                min_idx = j;
+            }
+        }
+        // Swap the found minimum element with the first element
+        temp = arr[min_idx];
+        arr[min_idx] = arr[i];
+        arr[i] = temp;
+    }
+}
+
+int main() {
+    int arr[] = {12, 11, 13, 5, 6, 7};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    pid_t pid = fork();
+
+    if (pid < 0) {
+        fprintf(stderr, "Fork failed\n");
+        return 1;
+    } else if (pid == 0) {
+        // Child process
+        printf("Child process sorting...\n");
+        selectionSort(arr, n);
+        printf("Child process sorted array:\n");
+        for (int i = 0; i < n; i++) {
+            printf("%d ", arr[i]);
+        }
+        printf("\n");
+        printf("Child process exiting.\n");
+        exit(0);
+    } else {
+        // Parent process
+        printf("Parent process waiting for child...\n");
+        int status;
+        waitpid(pid, &status, 0);  // Wait for the child process to complete
+
+        // Sorting in parent process using a different algorithm (Selection Sort Descending here)
+        printf("Parent process sorting...\n");
+        selectionSortDesc(arr, n);
+        printf("Parent process sorted array:\n");
+        for (int i = 0; i < n; i++) {
+            printf("%d ", arr[i]);
+        }
+        printf("\n");
+        printf("Parent process exiting.\n");
+    }
+
+    return 0;
+}
